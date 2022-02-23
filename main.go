83e42cbc74c29060a45a9b0e0a70ff542921e53c
rusk <lordruskk@gmail.com>
2022-02-23 15:44:28 -0800

    first try at implementing a signal handler in C

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
+// #include <signal.h>
+//
+// extern void runFromC(int, int);
+//
+// static void sighandler(int signum, siginfo_t *si, void *ucontext)
+// {
+//	int signal = signum - SIGRTMIN;
+//	int button = si->si_value.sival_int;
+//	runFromC(signal, button);
+// }
+//
+// static void addSig(int sig) {
+// 	struct sigaction sa = { .sa_sigaction = sighandler, .sa_flags = SA_SIGINFO|SA_ONSTACK };
+//	sigaction(sig, &sa, NULL);
+// }
+import "C"
+
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"os/signal"
+	// "os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -20,12 +39,13 @@ type block struct {
 	upInt int
 	upSig int
 
-	args []string // used internally
-	pos  int      // used internally
+	args []string
+	pos  int
 }
 
 var updateChan = make(chan struct{})
 var barBytesArr = make([][]byte, len(blocks))
+var signalMap = make(map[os.Signal][]block)
 
 func (b *block) run() {
 	outputBytes, err := exec.Command(b.args[0], b.args[1:]...).Output()
@@ -38,43 +58,66 @@ func (b *block) run() {
 	updateChan <- struct{}{}
 }
 
+func (b *block) runBB(button int) {
+	cmd := exec.Command(b.args[0], b.args[1:]...)
+	cmd.Env = append(cmd.Env, "BLOCK_BUTTON="+strconv.Itoa(button))
+	outputBytes, err := cmd.Output()
+	if err != nil {
+		log.Printf("Failed to update `%s`: %s\n", b.cmd, err)
+		return
+	}
+
+	barBytesArr[b.pos] = bytes.TrimSpace(bytes.Split(outputBytes, []byte("\n"))[0])
+	updateChan <- struct{}{}
+}
+
+//export runFromC
+func runFromC(signal, button C.int) {
+	fmt.Printf("signal: %d; button: %d\n", signal, button)
+	sig, butt := int(signal), int(button)
+	for _, b := range signalMap[syscall.Signal(sig)] {
+		if butt == 0 {
+			b.run()
+		} else {
+			b.runBB(butt)
+		}
+	}
+}
+
 func main() {
-	x, err := xgb.NewConn() // connect to X
+	x, err := xgb.NewConn()
 	if err != nil {
 		log.Fatalf("Cannot connect to X: %s\n", err)
 	}
 	defer x.Close()
 	root := xproto.Setup(x).DefaultScreen(x).Root
 
-	sigChan := make(chan os.Signal, 1024)
-	signalMap := make(map[os.Signal][]block)
-
 	for i := 0; i < len(blocks); i++ { // initialize blocks
-		go func(i int) {
-			blocks[i].pos = i
+		blocks[i].pos = i
 
-			if blocks[i].inSh {
-				blocks[i].args = []string{shell, cmdstropt, blocks[i].cmd}
-			} else {
-				blocks[i].args = strings.Split(blocks[i].cmd, " ")
-			}
+		if blocks[i].inSh {
+			blocks[i].args = []string{shell, cmdstropt, blocks[i].cmd}
+		} else {
+			blocks[i].args = strings.Split(blocks[i].cmd, " ")
+		}
 
-			if blocks[i].upSig != 0 {
-				signal.Notify(sigChan, syscall.Signal(34+blocks[i].upSig))
-				signalMap[syscall.Signal(34+blocks[i].upSig)] = append(signalMap[syscall.Signal(34+blocks[i].upSig)], blocks[i])
-			}
+		if blocks[i].upSig != 0 {
+			C.addSig(C.int(blocks[i].upSig))
+			signalMap[syscall.Signal(34+blocks[i].upSig)] = append(signalMap[syscall.Signal(34+blocks[i].upSig)], blocks[i])
+		}
 
-			blocks[i].run() // initially build bar
-			if blocks[i].upInt != 0 {
+		blocks[i].run()
+		if blocks[i].upInt != 0 {
+			go func(i int) {
 				for {
 					time.Sleep(time.Duration(blocks[i].upInt) * time.Second)
 					blocks[i].run()
 				}
-			}
-		}(i)
+			}(i)
+		}
 	}
 
-	go func() { // update bar on signal
+	go func() {
 		var finalBytesBuffer bytes.Buffer
 		for range updateChan {
 			for i := 0; i < len(blocks); i++ {
@@ -90,12 +133,5 @@ func main() {
 		}
 	}()
 
-	for sig := range sigChan { // handle signals
-		go func(sig *os.Signal) {
-			bs := signalMap[*sig]
-			for _, b := range bs {
-				go b.run()
-			}
-		}(&sig)
-	}
+	select {}
 }
