29d3464b926b6249a98e405bba08ee72c30b1a20
rusk <lordruskk@gmail.com>
2022-02-25 09:24:31 -0800

    info added

--- def.c
+++ def.c
@@ -4,6 +4,7 @@ will help with further development.
 
 issues:
 https://github.com/golang/go/issues/20639 <- using //extern|//export funcToCFromGo
+	https://golang.org/cmd/cgo/#hdr-C_references_to_Go
 https://www.cprogramming.com/declare_vs_define.html <- trying to get around ^^ fuckery
 https://www.tutorialspoint.com/cprogramming/c_functions.htm <- again trying to get around the fuckery above
 https://man7.org/linux/man-pages/man2/sigaction.2.html <- manpage for C signal handling that I need to implement
@@ -18,7 +19,9 @@ https://documentation.help/Golang/cgo.html < cgo basics
 https://people.kth.se/~johanmon/ose/assignments/signals.pdf <- nice pdf about C signals
 */
 
-// see https://golang.org/cmd/cgo/#hdr-C_references_to_Go
+/* from https://pkg.go.dev/cmd/cgo#hdr-C_references_to_Go:
+Using //export in a file places a restriction on the preamble: since it is copied into two different C output files, it must not contain any definitions, only declarations. If a file contains both definitions and declarations, then the two output files will produce duplicate symbols and the linker will fail. To avoid this, definitions must be placed in preambles in other files, or in C source files.
+*/
 #include <signal.h>
 
 /*
